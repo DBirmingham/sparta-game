3abe48120ae6bf7bbe2fdd0be3468ab67debcd81
Tech-A03 <tech-a03@MacBook-Air-9.local>
2018-02-28 15:50:43 +0000

    minor refactoring and revision of ending

--- project/js/index.js
+++ project/js/index.js
@@ -8,18 +8,33 @@ $(function(){
 	function init(){
 		$('.menu').css({'visibility': 'visible'})
 		$('#play').click(function(){
-			$('.menu').css({'display': 'none'})
+			$('.menu').css({'visibility': 'hidden'})
 			gameStart();
 		})
 
 		$('#instructions').click(function(){
-			$('.menu').html('<p>Player 1: w</p><p>Player 2: o</p><p>Mash the keys to outdo your opponent. Victory is given to the one who\'s covered the most distance unless one player is decisively faster than the other at the time of collision, so watch out for comebacks!</p>');
+			var oldHTML = $('.menu').html()
+			$('.menu').html('<p id="X" style="text-align: left">X</p><p>Player 1: w</p><p>Player 2: o</p><p>Mash the keys to outdo your opponent. Victory is given to the one who\'s covered the most distance unless one player is decisively faster than the other at the time of collision, so watch out for comebacks!</p>');
+			$('#X').on('click', function(){
+				$('.menu').html(oldHTML);
+				init();
+			})
+			$(window).on('keydown', function(event){
+				switch (event.key){
+				case 'Escape':
+					$('.menu').html(oldHTML);
+					init();
+					$(window).off('keydown');	
+				break;
+				}
+			})
 		})
 	}
 
 	function gameStart(){
+		var standStill = setInterval(stay, 200)
+		var pedometer = setInterval(counts, 500)
 		var sprite = 1
-		var end = false
 		$(window).on('keyup', function(event){
 			switch (event.key){
 			case 'w':
@@ -33,32 +48,55 @@ $(function(){
 			++counter2;
 			spriteChange2();
 			$('#Character2').css('right', num2 + '%');
+			// console.log($(window).width()/100)
 			break;
 			};
 			if ($('#Character2').position().left <  ($('#Character1').position().left + $('#Character1').width())) {
 				$(window).off('keyup')
-				gameEnd();
+				clearInterval(standStill)
+				clearInterval(pedometer)
+				if (counter1 > counter2 + 1) {
+					p1Win.sprite1
+					p1Win.sprite2
+					$('.menu').html('<h1>Player1 wins Decisively!!</h1><p>To the winner go the spoils</p><p>Play Again?</p><p>Main Menu</p>')
+					$('.menu').css({'visibility': 'visible'})
+				} else if (counter2 > counter1 + 1) {
+					p2Win.sprite1
+					p2Win.sprite2
+					$('.menu').html('<h1>Player2 wins Decisively!!</h1><p>To the winner go the spoils</p><p>Play Again?</p><p>Main Menu</p>')
+					$('.menu').css({'visibility': 'visible'})
+				} else if (counter1 == counter2 || counter1 == counter2 + 1 || counter2 == counter1 + 1) {
+					if (num1 > num2) {
+						p1Win.sprite1
+						p1Win.sprite2
+						$('.menu').html('<h1>Player1 wins!</h1><p></p><p>Play Again?</p><p>Main Menu</p>')
+						$('.menu').css({'visibility': 'visible'})
+					} else if (num2 > num1) {						
+						p2Win.sprite1
+						p2Win.sprite2
+						$('.menu').html('<h1>Player2 wins!</h1><p>To the winner go the spoils</p><p>Play Again?</p><p>Main Menu</p>')
+						$('.menu').css({'visibility': 'visible'})
+					} else {
+						alert('A stunning draw!!');
+						$('.menu').html('<h1>NO CONTEST</h1><p>Play Again?</p><p>Main Menu</p>')
+						$('.menu').css({'visibility': 'visible'})
+					}
+				}
+				// gameEnd();
 			}
 		});
 
 		function speed (){
-			if(end){
-				return
-			} else {
-				setInterval(counts, 500);
-			}
+			pedometer;
 		}
 
 		function still (){
-			if(end){
-				return
-			} else {
-				setInterval(stay, 200);
-			}
+			standStill;
 		}
 
 		function counts (){
 			console.log(counter1)
+			console.log(counter2)
 			if (counter1 !== 0 || counter2 !== 0){
 				counter1 = 0
 				counter2 = 0	
@@ -90,32 +128,25 @@ $(function(){
 			}
 		}
 
+		var p1win = {
+			sprite2: $('#sprite2').attr('src','img/Ending/Defeat21.png'),
+			sprite1: $('#sprite1').attr('src','img/Ending/Victory1.png')
+		}
+
+		var p2win = {
+			sprite2: $('#sprite2').attr('src','img/Ending/Victory21.png'),
+			sprite1: $('#sprite1').attr('src','img/Ending/Defeat1.png')
+		}
+
 		speed();
 		still();
 	}
 
-	function gameEnd(){
-		$('.container').animate({opacity:0}, 3000, function(){
-			$('.container').html("<img src='img/Ending/endscreenDemo.png'>")
-			$('.container').animate({opacity:1}, 3000, function(){
-				if (counter1 > counter2 + 1) {
-					alert('Player1 wins Decisively!')
-					$('#sprite2').attr('src','img/charSword/Death21.png')
-				} else if (counter2 > counter1 + 1) {
-					alert('Player2 wins Decisively')
-					$('#sprite1').attr('src','img/charSword/Death1.png')
-				} else if (counter1 == counter2) {
-					if (num1 > num2) {
-						alert('Player1 wins!')
-						$('#sprite2').attr('src','img/charSword/Death21.png')
-					} else if (num2 > num1) {
-						alert('Player2 wins!')
-						$('#sprite1').attr('src','img/charSword/Death1.png')
-					} else {
-						alert('A stunning draw!!')
-					}
-				}
-			})
-		});
-	}
+	// function gameEnd(num){
+	// 	$('.container').animate({opacity:0}, 3000, function(){
+	// 		$('.container').animate({opacity:1}, 3000, function(){
+				
+	// 		})
+	// 	});
+	// }
 });
